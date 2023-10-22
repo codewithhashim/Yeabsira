142e78a04ddb630486d57d61fe24aabe3a9a9047
prohashim <prohashim4e@gmail.com>
2023-10-23 00:00:38 +0500

    class 1 done

--- class1/app.py
+++ class1/app.py
@@ -1,1 +1,53 @@
-print("Helllloooo this is coding, the fun way :D ")
+# print("Helllloooo this is coding, the fun way :D ")
+
+# identifiers (This represents the name of the variable)
+greetings = "hiiiiiiiiiiiii"
+
+# Storage units for data in computer's memory
+#  OR named memory location is called a variable.
+
+# assignment operator (=)
+name_of_the_variable = "value of the variable"
+
+# Variable Assignment:  Assigning a value to a variable. (age = 454)
+# age is the name of the variable which we will call an identifier
+# assignment operator (=)
+# value of the variable
+age = 454
+
+#  Reassigning Variables:  Changing the value stored in a variable.
+age = 124
+
+# Return Value:  Output of a variable (function) that can be stored or used in calculations.
+
+# There is no return statement used for variables and we do it for functions instead but more on that later in the sessions
+total_age = age + age
+
+
+# Function: A block of code that we can reuse that either is already written or we can write on our own to use it over and over each time we want to use it OR if it is needed.
+
+age = 10
+# print function
+# in front of functions we will have () parenthesis.
+# print(age)
+
+age = 33
+# print(age)
+
+age = 45
+# print(age)
+
+# we are re assigning the age variable to an input function.
+age = input("What is your age mister? ")
+
+# printing multiple statements / expressions at one time. We ca print multiple variables as well.
+print("your age is: ", age)
+
+# printing multiple variables
+# print(age1, age2, age3, age4, ...............................)
+
+# operations on variables.
+a = 34
+b = 45
+c = a + b
+print(c)
