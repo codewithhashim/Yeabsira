a6be5def181b76bc4867ee5d2be6ec12c8d281c6
prohashim <prohashim4e@gmail.com>
2023-11-13 01:07:34 +0500

    data types done

---
+++ class3/app.py
@@ -0,0 +1,53 @@
+# 3name = "hwllo"
+
+_hello = "Hiiiiiii"
+
+hello454hello = 45
+
+# case-sensitivity
+
+# this is variable number 4
+hello = 2342
+
+# this is 5th
+Hello = 56
+
+
+# snake_case
+names_i = "hellooo"
+
+# you cannot leave a space between two words of the same variable name.
+hello_name = 34 # error if space is been used
+
+# camelCaseHere
+
+# ThisIsPascalCase
+
+
+# Data Type: The type of data that we are storing in a variable
+
+# sequence of Alphabets
+# Alex
+
+# #1 String data type
+my_name = "Alex"
+
+# Number / Integer data type
+my_age = 33
+#33
+
+# sequence of Alphabets
+# String -> same data type as Alex
+job_role = "Data scientist"
+# Data scientist
+
+# Decimal point
+# Float data type
+my_height = 5.11
+# 5.11
+
+# 100k -> True / False
+# Boolean
+is_100 = False
+
+is_lower = True
