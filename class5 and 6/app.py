b1e7ac1b8bb6802148c11652223885890bf7063d
prohashim <prohashim4e@gmail.com>
2023-12-03 21:48:59 +0500

    updated code after if statement

---
+++ class5 and 6/app.py
@@ -0,0 +1,35 @@
+# Line of code that does some specific task can be termed as statement.
+
+# conditions, rules that may set some kind of logic in our code
+# if you are 10 years old you can enter high school - condition for student
+
+# logic
+
+# conditional statements: statents that are based on some conditions
+
+#  if statement
+# syntax 
+
+# if condition:
+    # code here
+
+if 3 > 19:
+    print ("Yes")
+elif 45 > 55:
+    print("hellooo")
+elif 45 > 34:
+    print("yooooooo")
+else:
+    print("somethinggg elseeee")
+
+
+# Nested if statements
+
+if 4 > 1:
+    if 2>1:
+        print("you are right")
+
+
+# pass
+if 6 > 2:
+    pass
