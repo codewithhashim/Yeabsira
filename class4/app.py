e0ff972904576278b90c37ea04633c9972049242
prohashim <prohashim4e@gmail.com>
2023-12-03 20:37:15 +0500

    4th session

---
+++ class4/app.py
@@ -0,0 +1,61 @@
+#  + is an operator 
+# 4 are operands
+# 4 + 4
+
+# Operator, a symbol that can operate two operands depending on the symbol that we are using, addition, mul, div, sub
+
+# Arithmetic operator:
+# print( 4 + 5)
+# print( 4 - 5)
+# print( 4 * 5)
+# print( 4 / 5)
+
+
+# modulus operator?
+# remainder:
+# print( 9 % 2)
+# print( 91 % 2)
+
+
+# Exponentiation 
+# 2 * 2 * 2 = (2)3 
+# 2 ** 3
+print( 2 ** 3)
+
+# expression
+b=56
+a=56
+c=a+b
+print(c)
+
+# incrementing
+print( a + 1)
+
+# decrementing 
+print( a - 1)
+ab = 45
+
+# in other programming language incrementing
+# print(ab++)
+
+# in python 
+#  ab = 56
+# ab = ab + 1
+# ab = 57
+
+
+# ac = 34
+# ac = ac - 1
+# ac = 33
+
+
+# temprature = 4.5
+# decimal point
+# float
+
+
+# BODMAS 
+# MDAS
+
+# order of operations
+print (2 + 2 / 3 * 2 - (3*2))
