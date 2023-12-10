6fc37a1337999a6b6204622986635e3a10cf7fa4
prohashim <prohashim4e@gmail.com>
2023-12-10 22:28:48 +0500

    class 9 done

--- class7/app.py
+++ class7/app.py
@@ -11,11 +11,43 @@ bc = 56
 print(ab + bc / ab * bc)
 
 # Boolean Expression: Expressions that evaluate to either true or false.
-print(45 > 56)
+# print(45 > 56)
 
 # (==)
 # check if 2 is equal to 2 or not.
-print(2 == 2)
+# print(2 == 2)
 
 # (!=) check if something is not equal to another value.
-print(3 != 4)
+# print(3 != 4)
+
+# 1
+# And operator
+# That checks for two different coditions
+# In case Alex pass the test, and I fail it, in that case I need to work hard.
+
+# If I get there on time and I present the thesis well, I will be awarded by $100.
+
+#  if 1 is greater than 2 and 4 is greater than 1, print hello there.
+
+# checking two different conditions
+
+print(45 > 34 and 12 > 3)
+
+a = 45
+
+b = 34
+# equality operator used with And 
+if (a == 45 and b == 304):
+    print("Hellloooo there")
+# if both values are true it will produce a true value
+#  if both are false it will produce false
+# if one of them is true it will still produce false
+
+# Or operator
+if (a== 56 or b== 34):
+    print("printyyy")
+
+# it will produce true only if both are true or a single expression is true.
+# if both are false it will produc a false value
+
+
