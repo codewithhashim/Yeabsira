bc2884181d851a45bbbf7c85d016ec4d9ca2fb74
prohashim <prohashim4e@gmail.com>
2023-12-10 20:59:55 +0500

    class 7 concept 1

---
+++ class7/app.py
@@ -0,0 +1,21 @@
+# logical operators are used to perform logical operations
+#  if an apple is red, put that in a red basket.
+# if your names starts with A, you should pronounce your name a bit different.
+
+
+# an expression that invludes a logical operator is called a logical expression.
+# and, or, not
+
+ab = 45
+bc = 56
+print(ab + bc / ab * bc)
+
+# Boolean Expression: Expressions that evaluate to either true or false.
+print(45 > 56)
+
+# (==)
+# check if 2 is equal to 2 or not.
+print(2 == 2)
+
+# (!=) check if something is not equal to another value.
+print(3 != 4)
