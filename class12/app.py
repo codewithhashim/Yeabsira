48fd24eb78aa719854e6ded07a0f71503dfd4d88
prohashim <prohashim4e@gmail.com>
2024-01-14 22:30:15 +0500

    indentation in Python

---
+++ class12/app.py
@@ -0,0 +1,25 @@
+print("Hi, nice to meet you!")
+print("Hello")
+
+
+# Blocks of code.
+# Line/lines of code that is supposed to do one /multi tasks, but this code when we run it will run in one block.
+
+# if a block of code ends, the next block of code that we start will start from the very beginning of line such that there will be no indentation at all.
+
+# if it is a block of code where you are writing a (:) after the colon everything that you want to include in that block of code will be indented.
+
+# The next block starts from the very beginning of line, no space left at all.
+
+
+if 1==2:
+    print(2)
+    print(4)
+    print(6)
+
+if 6 == 6:
+    print(8)
+    print(8)
+    print(8)
+    print(8)
+    print(8)
