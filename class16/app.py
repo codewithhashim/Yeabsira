61ae4a242ff49276b130750f5745febfa61aac61
prohashim <prohashim4e@gmail.com>
2024-02-21 22:30:07 +0500

    class 16

---
+++ class16/app.py
@@ -0,0 +1,38 @@
+some_numbers = (23, 101, 1, 6)
+
+min_number = min(some_numbers)
+
+max_number = max(some_numbers)
+sorted_number = sorted(some_numbers, reverse=True)
+
+print("The minimum number is: ", min_number)
+print("The maximum number is: ", max_number)
+print(sorted_number)
+
+# Function ->  a block of organized, reusable code that is used to perform a single, related action. (Not official definition, just explaination)
+
+# syntax:
+
+# def is a keyword
+# function_name is the name of the function
+# () 
+def function_name():
+    # function body
+    print()
+
+
+def eat_pizza():
+    print("Lets eat pizza!")
+
+
+# function call / function invocation
+# invoke 
+eat_pizza()
+
+# # reusable
+# eat_pizza()
+# eat_pizza()
+# eat_pizza()
+# eat_pizza()
+# eat_pizza()
+
